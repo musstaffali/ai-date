78b4a64a03cba6793aac6f5899da59aeca84b851
Mustafa <mamusstaffali@gmail.com>
2023-09-21 10:48:58 -0400

    data file containing hotel properties

---
+++ src/data.js
@@ -0,0 +1,24 @@
+// src/data.js
+const hotels = [
+    {
+      id: 1,
+      name: 'Hotel A',
+      price: 100,
+      available: 5,
+    },
+    {
+      id: 2,
+      name: 'Hotel B',
+      price: 120,
+      available: 8,
+    },
+    {
+      id: 3,
+      name: 'Hotel C',
+      price: 80,
+      available: 10,
+    },
+  ];
+  
+  export default hotels;
+  
