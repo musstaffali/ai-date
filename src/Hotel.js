cd9f24babd55364b5ef3dbce6c3e2a751092d6e4
Mustafa <mamusstaffali@gmail.com>
2023-09-22 10:42:33 -0400

    hotel.js comment

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,11 +1,17 @@
 // src/Hotel.js
 import React from 'react';
 
+// The Hotel component displays information about a hotel
 function Hotel({ hotel }) {
   return (
     <div className="hotel">
+      {/* Hotel name */}
       <h2>{hotel.name}</h2>
+      
+      {/* Price per night */}
       <p>Price: ${hotel.price} per night</p>
+      
+      {/* Number of available rooms */}
       <p>Available Rooms: {hotel.available}</p>
     </div>
   );
